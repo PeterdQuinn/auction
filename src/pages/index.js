7919090741dd4fd1f35e885f6053e8dd85482cc8
diomededavid <david@d2creativedesign.com>
2024-01-21 16:32:55 -0700

    fixes tsconfig

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-   <h1>hello</h1>
-  
-  );
+  return <h1>hello</h1>
 }

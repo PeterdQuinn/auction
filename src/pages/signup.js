07417cf1260c5c86aaf1f5d6df028ae3d8679764
diomededavid <david@d2creativedesign.com>
2024-01-21 17:55:15 -0700

    adds form components

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import TextInput from '../../components/TextInput'
-const Login = () => {
+const signUp = () => {
   let form = useRef(null)
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -23,14 +23,27 @@ const Login = () => {
           <div className="pt-0 px-2 flex flex-col items-center justify-center">
             <h2 className="text-4xl leading-tight pt-8">The Auction</h2>
           </div>
-          <div className="pt-16 px-2 flex flex-col items-center justify-center">
+          <div className="pt-16 px-2 flex flex-col items-center justify-center gap-4">
             <h3 className="text-2xl sm:text-3xl xl:text-2xl font-bold leading-tight">
-              Login To Your Account
+              Create Your Account
             </h3>
           </div>
-          <TextInput label="Email" type="text" />
-          <TextInput label="password" type="password" />
 
+          <TextInput label="First Name" type="text" />
+          <TextInput label="Last Name" type="text" />
+          <TextInput label="Email" type="email" />
+          <TextInput label="Password" type="password" />
+          <div className="pt-6 w-full flex justify-between px-2 sm:px-6">
+            <div className="flex items-center">
+              <input id="rememberme" className="w-3 h-3 mr-2" type="checkbox" />
+              <label htmlFor="rememberme" className="text-xs">
+                Remember Me
+              </label>
+            </div>
+            <a className="text-xs" href="javascript: void(0)">
+              Forgot Password?
+            </a>
+          </div>
           <div className="px-2 sm:px-6">
             <button className="focus:outline-none w-full bg-white transition duration-150 ease-in-out hover:bg-gray-200 rounded text-indigo-600 px-8 py-3 text-sm mt-6">
               Login
@@ -47,4 +60,4 @@ const Login = () => {
     </section>
   )
 }
-export default Login
+export default signUp
